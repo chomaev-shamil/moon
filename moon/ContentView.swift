32561305e0829958d8fce87822e7c66bb0a31b7a
Shamil Chomaev <sham.chom11@gmail.com>
2022-07-21 18:05:07 +0300

    Initial Commit

---
+++ moon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moon
+//
+//  Created by Shamil Chomaev on 21.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ moon/moonApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonApp.swift
+//  moon
+//
+//  Created by Shamil Chomaev on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct moonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
